06b5fd9d6313bc5c8850bbf17fc16224e8ca3705
stevenlee87 <imepisode1@gmail.com>
2014-07-23 18:56:45 +0800

    add service and host statistic

--- aos/host/adminx.py
+++ aos/host/adminx.py
@@ -4,27 +4,32 @@ from __future__ import unicode_literals
 import xadmin
 
 from .models import (
-    Host, Group
+    Host, Service
 )
 
 
 class HostAdmin(object):
     reversion_enable = True
 
-    list_display = ('id', 'name', 'ip', 'group', 'update_time')
+    list_display = ('id', 'name', 'ip_in', 'ip_out', 'service', 'status', 'comment','update_time')
 
     list_display_links = ('id', 'name')
     list_editable = ('published', 'available')
 
-    search_fields = ('id', 'name', 'ip')
-    list_filter = ['group']
+    search_fields = ('id', 'name', 'ip_in', 'ip_out')
+    list_filter = ['service']
 
 
 xadmin.site.register(Host, HostAdmin)
+#xadmin.site.register(Host, )
 
 
-class GroupAdmin(object):
-    list_display = ('id', 'name', 'update_time')
+class ServiceAdmin(object):
+    list_display = ('id', 'name', 'host_count', 'update_time')
     list_display_links = ('id', 'name')
+    def host_count(self, obj):
+        return '%s台' % obj.host_set.count()
+    
+    host_count.short_description = '主机数量'	
 
-xadmin.site.register(Group, GroupAdmin)
+xadmin.site.register(Service, ServiceAdmin)
--- aos/host/models.py
+++ aos/host/models.py
@@ -4,8 +4,8 @@ from __future__ import unicode_literals
 from django.db import models
 
 
-class Group(models.Model):
-    name = models.CharField(max_length=200, verbose_name="名称")
+class Service(models.Model):
+    name = models.CharField(max_length=200, verbose_name="业务名称")
 
     update_time = models.DateTimeField(auto_now=True)
     created_time = models.DateTimeField(auto_now_add=True)
@@ -14,27 +14,30 @@ class Group(models.Model):
         return self.name
 
     class Meta:
-        verbose_name = verbose_name_plural = "主机组"
+        verbose_name = verbose_name_plural = "业务组"
 
 
 class Host(models.Model):
     HOST_STATUS = (
-        (0, '未使用'),
-        (1, '已使用'),
-        (2, '其他'),
+		(0, '在线'),
+        (1, '测试'),
+        (2, '空闲'),
+        (3, '其他'),
+		(4, '宿主机'),
     )
     name = models.CharField(max_length=200, verbose_name="主机名")
-    ip = models.IPAddressField()
+    ip_in = models.IPAddressField(verbose_name="内网IP")
+    ip_out = models.IPAddressField(blank=True, verbose_name="公网IP")
     status = models.IntegerField(choices=HOST_STATUS, verbose_name="状态")
-    note = models.CharField(max_length=1000, verbose_name="备注")
+    comment = models.CharField(max_length=1000, verbose_name="备注")
 
-    group = models.ForeignKey(Group)
+    service = models.ForeignKey(Service)
 
     update_time = models.DateTimeField(auto_now=True)
     created_time = models.DateTimeField(auto_now_add=True)
 
     def __unicode__(self):
-        return '<%s:%s:%s>' % (self.name, self.ip, self.status)
+        return '<%s:%s:%s>' % (self.name, self.ip_in, self.status)
 
     class Meta:
         verbose_name = verbose_name_plural = "主机"

efc4c7d100b4a906f3fedf59592b4b19f187f630
stevenlee87 <imepisode1@gmail.com>
2014-07-24 15:10:46 +0800

    add idc

--- aos/host/adminx.py
+++ aos/host/adminx.py
@@ -4,14 +4,14 @@ from __future__ import unicode_literals
 import xadmin
 
 from .models import (
-    Host, Service
+    Host, Service, InternetDataCenter
 )
 
 
 class HostAdmin(object):
     reversion_enable = True
 
-    list_display = ('id', 'name', 'ip_in', 'ip_out', 'service', 'status', 'comment','update_time')
+    list_display = ('id', 'name', 'ip_in', 'ip_out', 'internetdatacenter', 'service', 'type', 'status', 'comment','update_time')
 
     list_display_links = ('id', 'name')
     list_editable = ('published', 'available')
@@ -33,3 +33,13 @@ class ServiceAdmin(object):
     host_count.short_description = '主机数量'	
 
 xadmin.site.register(Service, ServiceAdmin)
+
+class InternetDataCenterAdmin(object):
+    list_display = ('id', 'name', 'idc_contact','host_count', 'comment', 'update_time')
+    list_display_links = ('id', 'name')
+    def host_count(self, obj):
+        return '%s台' % obj.host_set.count()
+
+    host_count.short_description = '主机数量'
+
+xadmin.site.register(InternetDataCenter, InternetDataCenterAdmin)
--- aos/host/models.py
+++ aos/host/models.py
@@ -14,8 +14,20 @@ class Service(models.Model):
         return self.name
 
     class Meta:
-        verbose_name = verbose_name_plural = "业务组"
+        verbose_name = verbose_name_plural = "业务管理"
 
+class InternetDataCenter(models.Model):
+    name = models.CharField(max_length=200, verbose_name="机房名称")
+    idc_contact = models.CharField(max_length=200, verbose_name="机房联系人")
+    comment = models.CharField(blank=True, max_length=1000, verbose_name="备注")
+    update_time = models.DateTimeField(auto_now=True)
+    created_time = models.DateTimeField(auto_now_add=True)
+
+    def __unicode__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = verbose_name_plural = "机房管理"
 
 class Host(models.Model):
     HOST_STATUS = (
@@ -25,13 +37,22 @@ class Host(models.Model):
         (3, '其他'),
 		(4, '宿主机'),
     )
+
+    HOST_TYPE = (
+        (0, '虚拟机'),
+        (1, '宿主机'),
+        (3, '物理机'),
+    )
     name = models.CharField(max_length=200, verbose_name="主机名")
     ip_in = models.IPAddressField(verbose_name="内网IP")
     ip_out = models.IPAddressField(blank=True, verbose_name="公网IP")
+
+    internetdatacenter = models.ForeignKey(InternetDataCenter, verbose_name="机房")
+    service = models.ForeignKey(Service, verbose_name="业务")
+
+    type = models.IntegerField(choices=HOST_TYPE, verbose_name="主机类型")
     status = models.IntegerField(choices=HOST_STATUS, verbose_name="状态")
     comment = models.CharField(max_length=1000, verbose_name="备注")
-
-    service = models.ForeignKey(Service)
 
     update_time = models.DateTimeField(auto_now=True)
     created_time = models.DateTimeField(auto_now_add=True)
@@ -40,4 +61,4 @@ class Host(models.Model):
         return '<%s:%s:%s>' % (self.name, self.ip_in, self.status)
 
     class Meta:
-        verbose_name = verbose_name_plural = "主机"
+        verbose_name = verbose_name_plural = "主机管理"
